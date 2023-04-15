2ba03360f94a4c40a7fcb8fe27779a130aa25f66
Nishantphule <nishantphule12@gmail.com>
2023-04-15 14:36:38 +0530

    outh

--- script.js
+++ script.js
@@ -91,7 +91,7 @@ function authenticate() {
     })
         .then(function(response) {
                 // Handle the results here (response.result has the parsed body).
-                console.log("Response", response.json());
+                console.log("Response", response);
               },
               function(err) { console.error("Execute error", err); });
   }
